c119e75a26da28700ce13fe21638dec1efca5f78
Michael-Batiuk <Michael.Batiuk@stu.ocsb.ca>
2025-09-19 13:30:39 -0400

    Shows temperature

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Michael
+ * Created on: Sep 2025
+ * This program Shows temperature
 */
 
-basic.showString('Hello, World!')
+// our variable for a temperature
+let temperatureCelsius: number = 0
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// Shows real temperature
+input.onButtonPressed(Button.A, function () {
+    temperatureCelsius = input.temperature()
+    basic.showString('The temperature is:' + (temperatureCelsius).toString())
+})
